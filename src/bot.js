121734a9a0d9efd9d4d155d09e8b67e1d323ea94
waterthis <140047359+waterthis@users.noreply.github.com>
2024-02-05 14:02:06 +0000

    changed to deploy mode

--- src/bot.js
+++ src/bot.js
@@ -13,22 +13,22 @@ const help_command = require("./commands/help.js");
 help_command(bot);
 
 
-//prod mode (Vercel)
-// export const startVercel = async (req, res) => {
-//   await production(req, res, bot);
-// };
+// prod mode (Vercel)
+export const startVercel = async (req, res) => {
+  await production(req, res, bot);
+};
 
-async function start_bot() {
-  try {
-    console.log("Starting bot. . .");
-    await bot.launch();
-  } catch (err) {
-    console.log("An Error Ocurred: Restarting Bot");
-    console.log(err);
-  }
-}
+// async function start_bot() {
+//   try {
+//     console.log("Starting bot. . .");
+//     await bot.launch();
+//   } catch (err) {
+//     console.log("An Error Ocurred: Restarting Bot");
+//     console.log(err);
+//   }
+// }
 
 /**********************************
   start bot is for local testing 
 *********************************/
-start_bot();
+// start_bot();
